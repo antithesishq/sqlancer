9f26265c4c1d6722434f6be574e100330e6f56bb
Isaac Chua <2425983+isaacchua@users.noreply.github.com>
2025-07-25 22:52:13 -0400

    Disabled MySQL show_old_temporals for versions 8+

--- src/sqlancer/mysql/gen/MySQLSetGenerator.java
+++ src/sqlancer/mysql/gen/MySQLSetGenerator.java
@@ -99,7 +99,11 @@ public class MySQLSetGenerator {
         SCHEMA_DEFINITION_CACHE("schema_definition_cache", (r) -> r.getLong(256, 524288), Scope.GLOBAL), //
         SHOW_CREATE_TABLE_VERBOSITY("show_create_table_verbosity", (r) -> Randomly.fromOptions("OFF", "ON"),
                 Scope.GLOBAL, Scope.SESSION), //
-        SHOW_OLD_TEMPORALS("show_old_temporals", (r) -> Randomly.fromOptions("OFF", "ON"), Scope.GLOBAL, Scope.SESSION),
+        /*
+         * Disabled because MySQL version 8+ no longer supports this and will raise an AssertionError
+         */
+        // SHOW_OLD_TEMPORALS("show_old_temporals", (r) -> Randomly.fromOptions("OFF", "ON"), Scope.GLOBAL,
+        // Scope.SESSION),
         /*
          * sort_buffer_size is commented out as a workaround for https://bugs.mysql.com/bug.php?id=95969
          */
